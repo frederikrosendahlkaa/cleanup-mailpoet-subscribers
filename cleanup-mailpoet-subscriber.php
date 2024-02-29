9c1f8e338959c579e897459c087c26ef14ab65b2
Frederik Rosendahl-Kaa <frederikrosendahlkaa@users.noreply.github.com>
2024-02-29 10:50:06 +0100

    Add cleanup-mailpoet-subscriber.php plugin file with functions

---
+++ cleanup-mailpoet-subscriber.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Cleanup MailPoet subscriber
+ * Description: Delete MailPoet subscribers that is Unsubscribed
+ * Version: 1.0.0
+ * Author: Nordic Custom Made
+ * Author URI: https://nordiccustommade.dk
+ * Requires Plugins: mailpoet
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * https://github.com/mailpoet/mailpoet/tree/trunk/doc
+ */
+
+ 
